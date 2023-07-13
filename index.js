f7c2fb09b12ff04ae16948fc7586bee7a8cbd9cd
Saikat Chatterjee <csaikat66@gmail.com>
2023-07-13 18:45:50 +0530

    Drum project completed

---
+++ index.js
@@ -0,0 +1,47 @@
+var numberOfButton = document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfButton;i++){
+	document.querySelectorAll(".drum")[i].addEventListener(
+		"click", function clickButoon(){
+		var buttonHTML = this.innerHTML;
+		makeSound(buttonHTML);
+	});
+
+}
+document.addEventListener("keypress",function keybordClick(event) {
+    var value =event.key;
+    makeSound(value);    
+});
+function makeSound(inp){
+	switch(inp){
+			case "w":
+				var audio = new Audio("./sounds/crash.mp3");
+				audio.play();
+				break;
+			case "a":
+				var audio = new Audio("./sounds/kick-bass.mp3");
+				audio.play();
+				break;
+			case "s":
+				var audio = new Audio("./sounds/snare.mp3");
+				audio.play();
+				break;
+			case "d":
+				var audio = new Audio("./sounds/tom-1.mp3");
+				audio.play();
+				break;
+			case "j":
+				var audio = new Audio("./sounds/tom-2.mp3");
+				audio.play();
+				break;
+			case "k":
+				var audio = new Audio("./sounds/tom-3.mp3");
+				audio.play();
+				break;
+			case "l":
+				var audio = new Audio("./sounds/tom-4.mp3");
+				audio.play();
+				break;
+			default:
+				console.log("error")
+		}
+}
